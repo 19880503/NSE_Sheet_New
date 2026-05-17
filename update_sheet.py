05917845848651bda3f6c970bfff8c9eef170bce
mahendra <mahendra.singh200974@gmail.com>
2026-05-17 11:53:05 +0530

    Update update_sheet.py

--- update_sheet.py
+++ update_sheet.py
@@ -179,17 +179,11 @@ for i in range(7):
 if data_to_insert:
 
     try:
-
         worksheet.batch_clear(['A2:C251'])
-
         worksheet.update('A2', data_to_insert)
-
         ist_now = (datetime.utcnow() + timedelta(hours=5, minutes=30)).strftime('%d-%b %H:%M')
-
         status_msg = f"Data Date: {fetched_date_str} | Last Update: {ist_now} (IST)"
-
         worksheet.update('K2', [[status_msg]])
-
         print(f"SUCCESS: Sheet Updated successfully with Turnover Data for {fetched_date_str}!")
-except Exception as e:
+    except Exception as e:
         print(f"ERROR: Google Sheet अपडेट करने में समस्या आई: {e}")
